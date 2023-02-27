b7f00e1a7f7d5fd2576a34404557f44d54e9175d
Dima Stekhov <dimastekhov@gmail.com>
2023-02-27 22:09:13 +0200

    save commit to change device

--- js/script.js
+++ js/script.js
@@ -1,69 +1,23 @@
 'use strict'
 
-let menu = document.querySelector('.header__menu');
-let main = document.querySelector('.main');
-let isSwitchingTabs = false;
-let transitionTime = 300;
+let cards = document.querySelector('.markup__cards');
+let markup = document.querySelector('.markup');
+document.addEventListener('click', preview);
 
-menu.addEventListener('click', switchTabs);
+function preview(event) {
+	let button = event.target;
+	if (!button.classList.contains('preview-button')) return;
 
-async function switchTabs(event) {
-	let link = event.target;
-	if (!event.target.closest('.header__menu-link')) return;
-	event.preventDefault();
+	markup.classList.toggle('markup_preview');
+	markup.scrollIntoView({ block: 'start', behavior: 'smooth' });
+	document.body.style.overflow = 'hidden';
 
-	if (isSwitchingTabs) return;
+	let card = button.closest('.card');
 
-	let prevSelectedLink = menu.querySelector('.header__menu-link_selected');
-	if (prevSelectedLink == link) return;
-
-	let tabName = link.dataset.tabName;
-	if (!tabName) return;
-
-	isSwitchingTabs = true;
-	prevSelectedLink.classList.remove('header__menu-link_selected');
-	link.classList.add('header__menu-link_selected');
-
-	await hideTab();
-	await showTab();
-
-	isSwitchingTabs = false;
-
-	async function showTab() {
-		let selectedTab = main.querySelector(`[data-tab-body='${tabName}']`);
-
-		selectedTab.style.opacity = 0;
-		selectedTab.hidden = false;
-
-		await render();
-		selectedTab.style.transition = `opacity ${transitionTime / 1000}s`;
-		await render();
-		selectedTab.style.opacity = '';
-
-		await render(transitionTime);
-		selectedTab.style.transition = '';
-	}
-
-	async function hideTab() {
-		let prevSelectedTab = main.querySelector('[data-tab-body]:not([hidden])');
-		prevSelectedTab.style.transition = `opacity ${transitionTime / 1000}s`;
-		prevSelectedTab.style.opacity = 0;
-
-		await render(transitionTime);
-
-		prevSelectedTab.hidden = true;
-		prevSelectedTab.style.transition = '';
-		prevSelectedTab.style.opacity = '';
-	}
 }
 
-function render(time) {
-	if (!time) return new Promise(resolve => requestAnimationFrame(resolve));
-	return new Promise(resolve => setTimeout(() => resolve(), time));
+function getCardNumber(card) {
+	return Array.from(cards).indexOf(card);
 }
 
-
-
-
-
-
+function 
