5387d7e5a0713b166f93e995c14b2194024c7415
Dmitry Stekhov <dimastekhov@gmail.com>
2023-02-28 00:22:38 +0200

    add part of preview mode add cards column add cards active classes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict'
 
 let cards = document.querySelector('.markup__cards');
+let currentCardScale = 1.2;
 let markup = document.querySelector('.markup');
 document.addEventListener('click', preview);
 
@@ -8,16 +9,25 @@ function preview(event) {
 	let button = event.target;
 	if (!button.classList.contains('preview-button')) return;
 
-	markup.classList.toggle('markup_preview');
+	document.querySelector('.card_selected')?.classList.remove('card_selected');
+
+	markup.classList.add('markup_preview');
 	markup.scrollIntoView({ block: 'start', behavior: 'smooth' });
 	document.body.style.overflow = 'hidden';
 
 	let card = button.closest('.card');
-
+	card.classList.add('card_selected');
+	setCenterCard(card);
 }
 
-function getCardNumber(card) {
-	return Array.from(cards).indexOf(card);
-}
+function setCenterCard(card) {
+	cards.style.top = '';
+	let prevCard = document.querySelector('.card_active');
+	if (prevCard) prevCard.classList.remove('card_active');
+	card.classList.add('card_active');
 
-function 
+	let currentTop = card.getBoundingClientRect().top;
+	let neededTop = (innerHeight - (card.offsetHeight * currentCardScale)) / 2;
+	let newCardsTop = neededTop - currentTop + 'px';
+	cards.style.top = newCardsTop;
+}
