561af4359a624fe597b7bffd8a92632cfa356079
Dima Stekhov <dimastekhov@gmail.com>
2023-02-28 17:41:05 +0200

    add cards swiping in preview mode

--- js/script.js
+++ js/script.js
@@ -1,33 +1,87 @@
 'use strict'
 
-let cards = document.querySelector('.markup__cards');
-let currentCardScale = 1.2;
-let markup = document.querySelector('.markup');
+const activeCardMargin = 70;
+let previewOn = false;
+let isSwitchingCards = false;
+
+
 document.addEventListener('click', preview);
+document.addEventListener('click', switchActiveCard);
 
 function preview(event) {
 	let button = event.target;
 	if (!button.classList.contains('preview-button')) return;
 
 	document.querySelector('.card_selected')?.classList.remove('card_selected');
-
-	markup.classList.add('markup_preview');
-	markup.scrollIntoView({ block: 'start', behavior: 'smooth' });
-	document.body.style.overflow = 'hidden';
-
 	let card = button.closest('.card');
 	card.classList.add('card_selected');
-	setCenterCard(card);
+
+	if (!previewOn) {
+		let markup = document.querySelector('.markup');
+		markup.classList.add('markup_preview');
+		markup.scrollIntoView({ block: 'start', behavior: 'smooth' });
+		document.body.style.overflow = 'hidden';
+
+		let cards = document.querySelector('.markup__cards');
+		cards.style.top = '';
+		setCenterCard(card);
+
+		previewOn = true;
+	}
 }
 
 function setCenterCard(card) {
-	cards.style.top = '';
+	let cards = document.querySelector('.markup__cards');
 	let prevCard = document.querySelector('.card_active');
-	if (prevCard) prevCard.classList.remove('card_active');
-	card.classList.add('card_active');
+	let cardsTop = cards.getBoundingClientRect().top;
 
 	let currentTop = card.getBoundingClientRect().top;
-	let neededTop = (innerHeight - (card.offsetHeight * currentCardScale)) / 2;
-	let newCardsTop = neededTop - currentTop + 'px';
-	cards.style.top = newCardsTop;
+	let neededTop = (innerHeight - (card.offsetHeight + activeCardMargin * 2)) / 2;
+
+	if (prevCard?.nextElementSibling == card) {
+		let compensationSize = activeCardMargin * 2;
+		currentTop -= compensationSize;
+	}
+
+	let newCardsTop = neededTop - currentTop;
+	cards.style.top = newCardsTop + cardsTop + 'px';
+	cards.addEventListener('transitionend', () => isSwitchingCards = false, { once: true });
+
+	prevCard?.classList.remove('card_active');
+	card.classList.add('card_active');
+
+	updateButtons(card);
+}
+
+function switchActiveCard(event) {
+	let button = event.target.closest('.markup__cards-button');
+	if (!button) return;
+	if (isSwitchingCards) return;
+
+	isSwitchingCards = true;
+	let direction = button.classList.contains('markup__cards-button_bottom') ? 'bottom' : 'top';
+	let activeCard = document.querySelector('.card_active');
+
+	if (direction == 'bottom') {
+		setCenterCard(activeCard.nextElementSibling);
+	} else if (direction == 'top') {
+		setCenterCard(activeCard.previousElementSibling);
+	}
+}
+
+function updateButtons(card) {
+	let cards = document.querySelector('.markup__cards');
+
+	let isFirstCard = card == cards.firstElementChild;
+	let isLastCard = card == cards.lastElementChild;
+
+	let topButton = document.querySelector('.markup__cards-button_top');
+	let bottomButton = document.querySelector('.markup__cards-button_bottom');
+	topButton.hidden = bottomButton.hidden = false;
+
+	if (isFirstCard) {
+		topButton.hidden = true;
+	} else if (isLastCard) {
+		bottomButton.hidden = true;
+	}
 }
