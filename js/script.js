574cc45c4d0f006c37e5b6b86cef66b6e07a567f
Dima Stekhov <dimastekhov@gmail.com>
2023-03-01 21:40:37 +0200

    uncomment code

--- js/script.js
+++ js/script.js
@@ -111,9 +111,9 @@ function switchPreviewDevice(event) {
 	deviceBlock.classList.add(`preview-block__device_${button.dataset.device}`);
 }
 
-/* let iframe = document.querySelector('.preview-block__iframe');
+let iframe = document.querySelector('.preview-block__iframe');
 
 iframe.onload = function () {
 	iframe.contentDocument.head.insertAdjacentHTML('afterbegin', '<link rel="stylesheet" href="https://distom.github.io/Portfolio/css/macOSScrollbar.css">');
 	iframe.contentDocument.body.classList.add('scrollbar');
-} */
+}
